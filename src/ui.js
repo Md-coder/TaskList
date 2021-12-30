d920a656d42035bb799f6a817d28aa989fd655e4
Md-coder <davidmadu24@gmail.com>
2021-12-30 14:17:46 +0100

    adding item to lists

--- src/ui.js
+++ src/ui.js
@@ -1,16 +1,59 @@
-import React from "react";
+import { keyboard } from "@testing-library/user-event/dist/keyboard";
+import React, { createRef } from "react";
 
 class Ui extends React.Component {
+  // constructor
+  constructor(props) {
+    super(props);
+
+    //     creating ref to communicate with the DOM element
+    this.inputRef = React.createRef();
+
+    // setting the state
+    this.state = {
+      lists: [],
+    };
+
+    //    bindinding this to the addList to have access to the this outside the constructor
+    this.addLists = this.addLists.bind(this);
+  }
+
+  //   creating the addList function
+  addLists(e) {
+    if (this.inputRef.current.value !== "") {
+      let newList = {
+        // adding new text
+        text: this.inputRef.current.value,
+        // add a random key ...we can use the date function or Math.random function
+        key: Date.now(),
+      };
+
+      this.setState((prevState) => {
+        return {
+          lists: prevState.lists.push(newList),
+        };
+      });
+      this.inputRef.current.value = "";
+    }
+    // prvent default loading
+    e.preventDefault();
+  }
+
   render() {
     return (
-      <div class="container mt-5">
+      <div className="container mt-5">
         <header>
-          <h2 class="text-center">Simple List</h2>
+          <h2 className="text-center">Simple List</h2>
         </header>
-        <div class="form text-center">
-          <form action="" class="input-group">
-            <input type="text" class="form-control" placeholder="add item" />
-            <button type="submit" class="button btn btn-primary">
+        <div className="form text-center">
+          <form action="" className="input-group" onSubmit={this.addLists}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="add item"
+              ref={this.inputRef}
+            />
+            <button type="submit" className="button btn btn-primary">
               Add
             </button>
           </form>
